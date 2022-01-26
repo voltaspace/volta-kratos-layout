9dc9fff6183cd1ffd996a777f4a83197c68fbf0c
volta <806269155@qq.com>
2022-01-26 14:47:50 +0800

    Delete internal/constant directory

--- internal/constant/sign.go
+++
@@ -1,6 +0,0 @@
-package constant
-
-const (
-	FostwxAesKey string = "2839XXXX48471427"
-	FostwxAesIv  string = "2839101048471427"
-)
--- internal/constant/table/table.go
+++
@@ -1,8 +0,0 @@
-package table
-
-const (
-	PanFosterwxMobile         = "pan_fosterwx_mobile"
-	PanFosterwxMobileWxFriend = "pan_fosterwx_mobile_wxfriend"
-	PanFosterwxStation        = "pan_fosterwx_station"
-	PanFosterwxTaskSchedule   = "pan_fosterwx_task_schedule"
-)
